6818a231f1f20a1f30f20b34910d041522151ae5
Pouyan <pouyan.zarbafian@hotmail.com>
2023-11-13 17:59:47 -0500

    project initialization

---
+++ src/context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleDarkTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", isDarkTheme);
+  };
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
