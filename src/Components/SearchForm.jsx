ddb4ace317c0d554872a2f1b968b693adb07195c
Pouyan <pouyan.zarbafian@hotmail.com>
2023-11-14 20:04:29 -0500

    create search form

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function SearchForm() {
-  return <div>SearchForm</div>;
-}
+const SearchForm = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target.elements.search.value);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default SearchForm;
